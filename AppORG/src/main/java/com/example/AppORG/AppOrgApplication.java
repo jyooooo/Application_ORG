d9075294a2e670431226103feddb42b505e6b8b6
jyooooo <0vf60m373241b3j@ezweb.ne.jp>
2021-08-28 09:20:02 +0900

    First commit

---
+++ AppORG/src/main/java/com/example/AppORG/AppOrgApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppORG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOrgApplication.class, args);
+	}
+
+}
---
+++ AppORG/src/test/java/com/example/AppORG/AppOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppORG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
